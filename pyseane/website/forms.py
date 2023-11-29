22775e321c0954ed0411653365560701b05aa858
Jeremi-Epita <jeremi.dessert@epita.fr>
2023-11-29 02:19:27 +0100

    translate email page, fix javascript

--- pyseane/website/forms.py
+++ pyseane/website/forms.py
@@ -75,12 +75,12 @@ class EmailForm(forms.Form):
         ("0", "Mon propre texte"),
         ("1", "Netflix")
     ]
-    mailtype = forms.CharField(label='Email expedition', widget=forms.Select(choices=MAIL_TYPE_CHOICES))
+    mailtype = forms.CharField(label='Mail envoyeur', widget=forms.Select(choices=MAIL_TYPE_CHOICES))
     mail = forms.CharField(label='Mail', required=False, max_length=100)
     password = forms.CharField(label='Password', required=False, max_length=100, widget=forms.PasswordInput)
-    name = forms.CharField(label='Name', max_length=100, required=True, initial='')
-    receiver = forms.CharField(label='Receiver', widget=forms.Textarea, required=True, initial='')
+    name = forms.CharField(label='Nom à afficher', max_length=100, required=True, initial='')
+    receiver = forms.CharField(label='Liste des cibles', widget=forms.Textarea(attrs={'class': 'clear-on-click'}), required=True, initial='Un mail par ligne exemple :\njon.doe@google.fr\nalice.bob@google.fr')
     template = forms.CharField(label='Mail template', widget=forms.Select(choices=CONTENT_TEMPLATE))
 
-    subject = forms.CharField(label='Subject', max_length=100, required=True, initial='')
-    content = forms.CharField(label='Content', widget=forms.Textarea, required=True, initial='')
+    subject = forms.CharField(label='Corps', max_length=100, required=True, initial='')
+    content = forms.CharField(label='Contenu', widget=forms.Textarea, required=True, initial='')
