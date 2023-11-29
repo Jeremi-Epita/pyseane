c8e3f895692a993d71a95bf90c988342e029ab46
nmolina42 <58724725+nicolaas91@users.noreply.github.com>
2023-11-29 14:46:30 +0100

    mail finish

--- pyseane/website/forms.py
+++ pyseane/website/forms.py
@@ -75,14 +75,14 @@ class EmailForm(forms.Form):
         ("0", "Mon propre texte"),
         ("1", "Netflix")
     ]
-    mailtype = forms.CharField(label='Mail envoyeur', widget=forms.Select(choices=MAIL_TYPE_CHOICES))
-    mail = forms.CharField(label='Mail', required=False, max_length=100)
-    password = forms.CharField(label='Password', required=False, max_length=100, widget=forms.PasswordInput)
+    mailtype = forms.CharField(label='Mail envoyeur', widget=forms.Select(attrs={'class': 'form-control form-control-lg template-dropdown mx-left form-mailtype'},choices=MAIL_TYPE_CHOICES),)
+    mail = forms.CharField(label='Mail', required=False, max_length=100, initial='')
+    password = forms.CharField(label='Password', required=False, max_length=100, widget=forms.PasswordInput, initial='')
     name = forms.CharField(label='Nom à afficher', max_length=100, required=True, initial='')
     receiver = forms.CharField(label='Liste des cibles', widget=forms.Textarea(attrs={'class': 'clear-on-click'}), required=True, initial='Un mail par ligne exemple :\njon.doe@google.fr\nalice.bob@google.fr')
     template = forms.CharField(
     label='Mail template',
-    widget=forms.Select(attrs={'class': 'form-control form-control-lg template-dropdown mx-auto'}, choices=CONTENT_TEMPLATE),
+    widget=forms.Select(attrs={'class': 'form-control form-control-lg template-dropdown mx-left'}, choices=CONTENT_TEMPLATE),
     )
     subject = forms.CharField(label='Corps', max_length=100, required=True, initial='')
     content = forms.CharField(label='Contenu', widget=forms.Textarea, required=True, initial='')
